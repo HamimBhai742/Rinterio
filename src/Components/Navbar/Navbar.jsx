5570a157d7e8c0f7fe3ecb47a076291f437415ff
HamimBhai742 <omagootruelove147@gmail.com>
2024-10-31 10:23:58 +0600

    Works on Start Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="mt-3">
+      <div className="flex gap-2 items-center hover:cursor-pointer">
+        <figure className="bg-[#ABEF5F] p-2 rounded-full">
+          <img className="" src="../../../images/Group 1.png" alt="" />
+        </figure>
+        <h3 className="text-3xl font-extrabold">Rinterio</h3>
+      </div>
+      <div className="flex gap-3 items-center">
+        <NavLink to="/" className="text-[#737373] text-lg">
+          Home
+        </NavLink>
+        <NavLink to="/services" className="text-[#737373] text-lg">
+          Services
+        </NavLink>
+        <NavLink to="/protfolio" className="text-[#737373] text-lg">
+          Portfolio
+        </NavLink>
+        <NavLink to="/blogs" className="text-[#737373] text-lg">
+          Blogs
+        </NavLink>
+        <NavLink to="/contact-us" className="text-[#737373] text-lg">
+          Contact Us
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const Root = () => {
-    return (
-        <div>
-            <Outlet/>
-        </div>
-    );
+  return (
+    <div className="w-[95%] mx-auto">
+      <Navbar />
+      <div className="my-6">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Root;
